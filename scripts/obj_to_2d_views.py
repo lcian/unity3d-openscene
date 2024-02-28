833a72e2f4d97e363158bec633bc3098b465c16b
lcian <cian.lorenzo@gmail.com>
2024-02-28 10:57:38 +0100

    feat: improve scripts

--- scripts/obj_to_2d_views.py
+++ scripts/obj_to_2d_views.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 # pyright: reportGeneralTypeIssues=false
 import torch
+import torchvision.transforms as transforms
 from pytorch3d.io import load_objs_as_meshes
 from pytorch3d.renderer import (
+    FoVPerspectiveCameras,
     look_at_view_transform,
-    FoVPerspectiveCameras, 
-    PointLights, 
-    RasterizationSettings, 
-    MeshRenderer, 
-    MeshRasterizer,  
+    MeshRasterizer,
+    MeshRenderer,
+    PointLights,
+    RasterizationSettings,
     SoftPhongShader,
     SoftPhongShader,
 )
-import torchvision.transforms as transforms
 
 if torch.cuda.is_available():
     device = torch.device("cuda:0")
@@ -20,77 +20,62 @@ if torch.cuda.is_available():
 else:
     device = torch.device("cpu")
 
+DEFAULT_IMAGE_SIZE = (1200, 680)  # same as Replica
+DEFAULT_TEXTURE_ATLAS_SIZE = 64  # works well for the examples
+DEFAULT_NUM_VIEWS = 16
+
 
 # from https://pytorch3d.org/tutorials/fit_textured_mesh
-# TODO: camera positioning
-def obj_to_2d_views(filename, image_size=256, num_views=20):
+def obj_to_2d_views(
+    filename,
+    image_size=DEFAULT_IMAGE_SIZE,
+    texture_atlas_size=DEFAULT_TEXTURE_ATLAS_SIZE,
+    num_views=DEFAULT_NUM_VIEWS,
+):
 
-    mesh = load_objs_as_meshes([filename], load_textures=True, create_texture_atlas=True, texture_atlas_size=512, device=device)
+    mesh = load_objs_as_meshes(
+        [filename],
+        load_textures=True,
+        create_texture_atlas=True,
+        texture_atlas_size=texture_atlas_size,
+        device=device,
+    )
 
-    # We scale normalize and center the target mesh to fit in a sphere of radius 1 
-    # centered at (0,0,0). (scale, center) will be used to bring the predicted mesh 
-    # to its original center and scale.  Note that normalizing the target mesh, 
-    # speeds up the optimization but is not necessary!
+    # center the mesh
     verts = mesh.verts_packed()
     N = verts.shape[0]
     center = verts.mean(0)
-    scale = max((verts - center).abs().max(0)[0])
     mesh.offset_verts_(-center)
-    #mesh.scale_verts_((1.0 / float(scale)))
 
-    # Get a batch of viewing angles. 
+    # generate viewing angles and cameras
     elev = torch.zeros(num_views)
     azim = torch.linspace(0, 360, num_views)
-
-    # Place a point light in front of the object.
-    #lights = PointLights(device=device, location=[[0.0, 0.0, -3.0]])
-
-    # Initialize an OpenGL perspective camera that represents a batch of different 
-    # viewing angles. All the cameras helper methods support mixed type inputs and 
-    # broadcasting. So we can view the camera from the a distance of dist=2.7, and 
-    # then specify elevation and azimuth angles for each viewpoint as tensors. 
     R, T = look_at_view_transform(dist=0.1, elev=elev, azim=azim)
     cameras = FoVPerspectiveCameras(device=device, R=R, T=T)
 
-    # We arbitrarily choose one particular view that will be used to visualize results
-    camera = FoVPerspectiveCameras(device=device, R=R[None, 1, ...], 
-                                      T=T[None, 1, ...]) 
+    # arbitrarily choose one particular view to be used for rasterizer and shader
+    camera = FoVPerspectiveCameras(device=device, R=R[None, 1, ...], T=T[None, 1, ...])
 
     raster_settings = RasterizationSettings(
-        image_size=(1200, 680), 
-        blur_radius=0.0, 
-        faces_per_pixel=1, 
-        #max_faces_per_bin = 50000,
+        image_size=(1200, 680),
+        blur_radius=0.0,
+        faces_per_pixel=1,
     )
 
-    # Create a Phong renderer by composing a rasterizer and a shader. The textured 
-    # Phong shader will interpolate the texture uv coordinates for each vertex, 
-    # sample from a texture image and apply the Phong lighting model
     renderer = MeshRenderer(
-        rasterizer=MeshRasterizer(
-            cameras=camera, 
-            raster_settings=raster_settings
-        ),
+        rasterizer=MeshRasterizer(cameras=camera, raster_settings=raster_settings),
         shader=SoftPhongShader(
-            device=device, 
+            device=device,
             cameras=camera,
-            #lights=lights
-        )
+        ),
     )
 
-    # Create a batch of meshes by repeating the cow mesh and associated textures. 
-    # Meshes has a useful `extend` method which allows us do this very easily. 
-    # This also extends the textures. 
+    # render
     meshes = mesh.extend(num_views)
-
-    # Render the cow mesh from each viewing angle
-    target_images = renderer(meshes, cameras=cameras)#, lights=lights)
-
-    # Our multi-view cow dataset will be represented by these 2 lists of tensors,
-    # each of length num_views.
+    target_images = renderer(meshes, cameras=cameras)
     target_rgb = [target_images[i, ..., :3] for i in range(num_views)]
-    #target_cameras = [FoVPerspectiveCameras(device=device, R=R[None, i, ...], T=T[None, i, ...]) for i in range(num_views)]
-    
+
+    # save
     transform = transforms.ToPILImage()
     for i, rgb in enumerate(target_rgb):
         img = transform(rgb.permute(2, 0, 1))
@@ -99,9 +84,28 @@ def obj_to_2d_views(filename, image_size=256, num_views=20):
 
 if __name__ == "__main__":
     import argparse
-    parser = argparse.ArgumentParser(description='Generate 2D views from 3D .obj')
-    parser.add_argument('filename', type=str, help='Input file')
-    parser.add_argument('--image_size', type=int, default=256, help='Image size')
-    parser.add_argument('--num_views', type=int, default=20, help='Number of views')
+
+    parser = argparse.ArgumentParser(description="Generate 2D views from 3D .obj")
+    parser.add_argument("filename", type=str, help="Input file")
+    parser.add_argument(
+        "--views_w", type=int, default=DEFAULT_IMAGE_SIZE[0], help="View width"
+    )
+    parser.add_argument(
+        "--views_h", type=int, default=DEFAULT_IMAGE_SIZE[1], help="View height"
+    )
+    parser.add_argument(
+        "--texture_atlas_size",
+        type=int,
+        default=DEFAULT_TEXTURE_ATLAS_SIZE,
+        help="View height",
+    )
+    parser.add_argument(
+        "--num_views", type=int, default=DEFAULT_NUM_VIEWS, help="Number of views"
+    )
     args = parser.parse_args()
-    obj_to_2d_views(args.filename, args.image_size, args.num_views)
+    obj_to_2d_views(
+        args.filename,
+        (args.views_w, args.views_h),
+        args.texture_atlas_size,
+        args.num_views,
+    )
